6911f0938b39e832cee7bc3663bdb757802721eb
jghuynh <jghuynh@dons.usfca.edu>
2019-04-10 17:58:57 -0700

    Thank you Chris! Fei chang fei chang xie xie ni!

--- BST.java
+++ BST.java
@@ -3,82 +3,81 @@
  * @param <T> the template
  * @author Justine Huynh
  */
-public class BST<T> implements Comparable<T> {
+public class BST<T>{
 
     // only node we need is root
-    private BSTNode<T> root;
+    private BSTNode root;
 
+    // every data in node is comparable. Type cast root.data into (T)
     /**
      * Constructs an Object of type BSTNode. Each root node has data, a left node, and a right node
-     * @param <T> the template
      */
-    public class BSTNode<T> {
-        private Comparable<T> data;
-        private BSTNode<T> left;
-        private BSTNode<T> right;
+    public class BSTNode {
+        private Comparable data;
+        private BSTNode left;
+        private BSTNode right;
 
         public BSTNode(Comparable value) {
             this.data = value;
         }
 
-
-    }
-
-    /**
-     * Compares a given value with the current node's value
-     * @param value the given value
-     * @return 0 if node's value and given value are equal; 1 if node's value is greater than
-     * given value; -1 if node's value is less than given value
-     */
-    public int data_compareTo(Comparable value) {
-        if (root.data == value) {
-            return 0;
+        public Comparable getData(){
+            return data;
         }
-        else if (root.data > value) {
-            return 1;
-        }
-        return -1;
-    }
 
+        public BSTNode getLeft() {
+            return left;
+        }
+
+        public BSTNode getRight() {
+            return right;
+        }
+        
+    }
 
     /**
      * Deletes a given value from the BST
      * @param value
      */
     public void delete(Comparable value) {
-        delete(root, value);
+        root = delete(root, value);
     }
 
-    private BSTNode<T> delete(BSTNode<T> node, Comparable value) {
+    /**
+     * Deletes a given value from the BST; private version of delete
+     * @param node the node that has the value
+     * @param value the value to delete
+     * @return the deleted node
+     */
+    private BSTNode delete(BSTNode node, Comparable value) {
         if (node == null) {
             return null;
         }
-        if (data_compareTo(value) == 0) { // match
+        if (node.data.compareTo(value) == 0) { // match
             if (node.left == null) {
                 return node.right;
             }
             else if (node.right == null) {
                 return node.left;
             }
-            else { // if both left and right child exists
-                if (node.right.left == null){
+            else if (node.right.left == null){
                     // if leftmost node on RHS is null
                     node.data = node.right.data; // adopt node's right data
                     node.right = node.right.right; // node's right child adopts the next right child
-                }
-                else {
-                    node.data = removeSmallest(node.right); // find IOS
-                }
+                    return node;
+            }
+            else {
+                node.data = removeSmallest(node.right); // find IOS
+                return node;
             }
         }
-        else if (data_compareTo(value) < 0) { // if node is too small, go right
-            node.right = delete(node.right, value);
-            return node.right;
+        else if (node.data.compareTo(value) > 0) { // if node is too small, go right
+            node.right = delete(node.left, value);
         }
-//        else {
-        node.left = delete(node.left, value);
-        return node.left;
-//        }
+        else {
+                delete(node.right, value);
+        }
+        return node;
 
     }
 
@@ -87,7 +86,7 @@ public class BST<T> implements Comparable<T> {
      * @param node the root node
      * @return the smallest node
      */
-    private Comparable removeSmallest(BSTNode<T> node) {
+    private Comparable removeSmallest(BSTNode node) {
         if (node.left.left == null) {
             Comparable smallest = node.left.data;
             node.left = node.left.right;
@@ -98,22 +97,25 @@ public class BST<T> implements Comparable<T> {
         }
     }
 
-
+    public boolean find(Comparable value){
+      return  find(root, value);
+    }
     /**
      * Finds a given value in the BST
-     * @param value
+     * @param root the node to start looking
+     * @param value the value to look for
      */
-    public boolean find(BSTNode<T> root, BSTNode<T> tempNode, Comparable value) {
+    public boolean find(BSTNode root, Comparable value) {
         if (root == null) {
             return false;
         }
-        if (data_compareTo (value) == 0) {
+        if (root.data.compareTo(value) == 0) {
             return true;
         }
-        else if (data_compareTo(value) > 0) {
-            return find(root, root.right, value);
+        else if (root.data.compareTo(value)  < 0) {
+            return find(root.right, value);
         }
-        return find(root, root.left, value);
+        return find(root.left, value);
     }
 
     /**
@@ -130,17 +132,18 @@ public class BST<T> implements Comparable<T> {
      * @param value the value to be compared to
      * @return the node
      */
-    private BSTNode<T> insert(BSTNode node, Comparable  value) {
+    private BSTNode insert(BSTNode node, Comparable value) {
         if (node == null) {
-            BSTNode<T> newNode = new BSTNode<T>(value);
+            BSTNode newNode = new BSTNode(value);
             return newNode;
         }
-        else if (data_compareTo(value) > 0) {
+        else if ( ( node.data).compareTo(value) < 0) {
             node.right = insert(node.right, value);
         }
-        else {
+        else if((node.data).compareTo(value) > 0){
             node.left = insert(node.left, value);
         }
+
         return node;
     }
 
@@ -149,19 +152,16 @@ public class BST<T> implements Comparable<T> {
      * Prints all the nodes in order
      */
     public void print() {
-        if (root != null) {
-            print(root.left); // go left recursively until you hit null
-        }
-
+            print(root); // go left recursively until you hit null
     }
 
     /**
      * Private version of print; prints all the nodes in order
      * @param node the node that we compare other nodes to
      */
-    private void print(BSTNode<T> node) {
-        if (root != null) {
-            print(root.left);
+    private void print(BSTNode node) {
+        if (node != null) {
+            print(node.left);
             System.out.println(node.data);
             print(node.right);
         }
